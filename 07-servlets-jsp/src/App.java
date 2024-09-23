531f161faf8eb86ad282d4816e63a59da601fd01
Gabriel Arroyo <gabriel2021arr@gmail.com>
2024-09-23 20:35:16 -0300

    eliminacion de archivo

--- 07-servlets-jsp/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("HOLA MUNDO..");
-
-        String test = "Test GIT";
-        System.out.println(test);
-    }
-}
